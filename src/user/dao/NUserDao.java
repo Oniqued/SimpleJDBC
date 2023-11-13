36ce5f380bb378f210a3b57b3290f60eaf312e0b
Oniqued <zktl3575@gmail.com>
2023-11-13 22:11:50 +0900

    프레임워크 없이 구현해보는 웹개발

---
+++ src/user/dao/NUserDao.java
@@ -0,0 +1,16 @@
+package user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NUserDao extends UserDao {
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/springbook", "root", "1234");
+
+        return c;
+    }
+}
